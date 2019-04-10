badef309eb2d0b76498009daef3903e03efc5dd6
Andrew Edson <docedson@gmail.com>
2019-04-10 14:20:07 -0500

    Add project files.

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Let the user input a number to determine even or odd
+
+            Console.WriteLine("Let's determine if your number is even or odd. Please enter a whole number: ");
+            string input = Console.ReadLine();
+            int number = Convert.ToInt32(input);
+
+            //Determine the remainder to see if it = 0 (an even #) or has one (odd #)
+            int remainder = number % 2;
+
+            if (remainder == 0)
+            {
+                Console.WriteLine("Your number of " +number +" is even");
+            }
+
+            else
+            {
+                Console.WriteLine("Your number of " + number + " is odd");
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
